99358acb351f7f1d82b8110f0b9cc28f780e33b7
RUBISCOMX <jigbadouin@gmail.com>
2023-03-28 20:33:05 -0700

    push

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 from config import Config
 from connector_analytics.analyticsV2 import zohoConnectV2
+import csv 
+import pandas as pd
+import sqlite3
 
 objZoho = zohoConnectV2(srvUrl=Config.SERVERAUTH,
 					tokenToRefresh=Config.REFRESHTOKEN,
 					clientId=Config.CLIENTID,
 					clientSecret=Config.CLIENTSECRET)
 
-sql_query = 'SELECT%20ID%20FROM%20bitacora'
+sql_query = 'SELECT%20usuario,ID%20FROM%20bitacora2'
 data = objZoho.zohoQuery(srvUrl='https://analyticsapi.zoho.com',
                                                 orgid='806718080', 
                                                 workspace='ITSON', 
-                                                table_name='bitacora',
+                                                table_name='bitacora2',
                                                 email=Config.LOGINEMAILID, 
                                                 sql_query=sql_query)
 print(data)
+
+
+
+
 
